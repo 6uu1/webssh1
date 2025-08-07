3606aceca97f271ea98c5f722dee16607fde0ae6
test <admin@test.com>
2025-08-07 11:39:30 +0800

    1

--- fetch_wireproxy.sh
+++ fetch_wireproxy.sh
@@ -3,19 +3,13 @@ set -e
 
 echo ">> Downloading wireproxy binary ..."
 
-# 1. 找出最新版本号
-TAG=$(curl -s https://api.github.com/repos/octeep/wireproxy/releases/latest \
-      | grep '"tag_name":' | cut -d '"' -f 4)
+# 直接拿 latest 版本的 Linux amd64 压缩包
+curl -L https://github.com/octeep/wireproxy/releases/latest/download/wireproxy_linux_amd64.tar.gz \
+  -o wireproxy_linux_amd64.tar.gz
 
-# 2. 对应的压缩包文件名
-TAR_FILE="wireproxy_linux_amd64.tar.gz"
-
-# 3. 下载并解压到当前目录
-curl -L "https://github.com/octeep/wireproxy/releases/download/${TAG}/${TAR_FILE}" \
-  -o "${TAR_FILE}"
-
-tar -xzf "${TAR_FILE}" wireproxy
+# 解包得到可执行文件
+tar -xzf wireproxy_linux_amd64.tar.gz wireproxy
 chmod +x wireproxy
-rm "${TAR_FILE}"
+rm wireproxy_linux_amd64.tar.gz
 
-echo ">> wireproxy downloaded: $(./wireproxy -v)"
+echo ">> wireproxy ready: $(./wireproxy -v)"
