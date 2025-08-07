dfc5107716d36f7e4177627133a32334fc878ca3
test <admin@test.com>
2025-08-07 11:49:02 +0800

    修正wireproxy配置文件名并添加默认端口设置

--- start.sh
+++ start.sh
@@ -6,7 +6,9 @@ sed "s|\${WARP_PRIVKEY}|${WARP_PRIVKEY}|g" \
     wgcf-profile.conf.template > wgcf-profile.conf
 
 # 启动 wireproxy
-./wireproxy -c wireproxy.json &
+./wireproxy -c wireproxy.conf &
 
 export ALL_PROXY="socks5h://127.0.0.1:1080"
+# 若 $PORT 为空，默认 8888
+PORT=${PORT:-8888}
 exec python run.py --port "$PORT" --address 0.0.0.0 --xheaders=False
