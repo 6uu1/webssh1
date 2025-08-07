6926bf119032abf7b2a2476e6cdadc70fa0f9de1
test <admin@test.com>
2025-08-07 12:06:40 +0800

    更新start.sh脚本，添加代理设置，支持socks5代理类型和主机配置

--- start.sh
+++ start.sh
@@ -10,4 +10,11 @@ sed "s|\${WARP_PRIVKEY}|${WARP_PRIVKEY}|g" \
 
 export ALL_PROXY="socks5h://127.0.0.1:1080"
 # 若 $PORT 为空，默认 8888
-exec python run.py --port=${PORT:-8888} --address=0.0.0.0 --xheaders=False
+#exec python run.py --port=${PORT:-8888} --address=0.0.0.0 --xheaders=False
+exec python run.py \
+  --port=${PORT:-8888} \
+  --address=0.0.0.0 \
+  --xheaders=False \
+  --proxytype=socks5 \
+  --proxyhost=127.0.0.1 \
+  --proxyport=1080
